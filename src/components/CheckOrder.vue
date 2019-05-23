792aea68087a4afa4c31817894a5a3ce9e94d3ae
unknown <15270013718@163.com>
2019-05-23 22:14:32 +0800

    表单

---
+++ src/components/CheckOrder.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="form">
+    <h1>Contact Form
+			<span>Please fill all the texts in the fields.</span>
+		</h1>
+		<label>
+			<span>Your Name :</span>
+			<input id="name" type="text" name="name" placeholder="Your Full Name" />
+		</label>
+		<label>
+			<span>商品名 :</span>
+			<input id="goodName" type="text" name="goodName" readonly="readonly" v-bind:value = "name" />
+		</label>
+        <label>
+			<span>价格 :</span>
+			<input id="price" type="text" name="price" readonly="readonly" v-bind:value = "price" />
+		</label>
+
+		
+		<label>
+			<span>Subject :</span><select name="selection">
+				<option value="Job Inquiry">Job Inquiry</option>
+				<option value="General Question">General Question</option>
+			</select>
+		</label>
+		
+</div>
+
+
+</template>
+<script>
+export default {
+    data(){
+        return{
+            username:"",
+            name: this.$route.query.name,
+            price:this.$route.query.price
+        }
+    }
+}
+</script>
+<style scoped>
+
+
+
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList'
+import CheckOrder from '@/components/CheckOrder';
 
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
-  routes: [
-    {
-      path: '/',
-      name: 'GoodsList',
-      component: GoodsList
-    }
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'GoodsList',
+    component: GoodsList
+  },
+  {
+    path: '/CheckOrder',
+    name: 'CheckOrder',
+    component: CheckOrder
+
+  }
   ]
 })
